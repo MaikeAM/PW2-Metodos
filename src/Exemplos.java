74681caadbc0838d62650cb73e5a0e33f740b6d3
Maike <morais.maike@gmail.com>
2020-03-04 20:37:52 -0300

    Exemplos de metodo

--- src/Exemplos.java
+++ src/Exemplos.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
 
 public class Exemplos {
+	private static Scanner in;
+
 	public static void main(String[] args) {
-		int idade = 19;
+       System.out.println("Digite o numero");
+		in = new Scanner( System.in );
+        int num = in.nextInt();
+ 
+	        
+		ehMaiorIdade(30);
+		classificar(num);
+	}
+
+	public static void ehMaiorIdade(int idade) {
 		if (idade >= 18) {
 			System.out.println("MAIOR DE IDADE");
-		} 
-		else {
+		} else {
 			System.out.println("MENOR DE IDADE");
 		}
+
 	}
+
+	public static void classificar(int num) {
+		if (num > 0) {
+			System.out.println("Positivo");
+		} else {
+			if (num == 0) {
+				System.out.println("ZERO");
+			} else {
+				System.out.println("Negativo");
+			}
+		}
+	}
+
 }
