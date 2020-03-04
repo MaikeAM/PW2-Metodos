f67e2a50776357eb5818855fa7801dbb50f73a83
Maike <morais.maike@gmail.com>
2020-03-04 20:02:39 -0300

    Revisão de if

---
+++ src/Exemplos.java
@@ -0,0 +1,12 @@
+
+public class Exemplos {
+	public static void main(String[] args) {
+		int idade = 19;
+		if (idade >= 18) {
+			System.out.println("MAIOR DE IDADE");
+		} 
+		else {
+			System.out.println("MENOR DE IDADE");
+		}
+	}
+}
